4f5841466888f5eb0f73a79f247d2f82873248f1
ClintFix <clint.fix@gmail.com>
2021-02-26 10:27:04 -0700

    installed yup, built formschema

---
+++ src/validation/formSchema.js
@@ -0,0 +1,18 @@
+import * as yup from 'yup'
+
+const formSchema = yup.object().shape({
+    name: yup.string()
+        .trim()
+        .required('Please give us a name for your order')
+        .min(2, 'Your name must be at least 2 characters'),
+    size: yup.number()
+        .required('Please choose a Pizza size'),
+    sauce: yup.string()
+        .required('Please choose a sauce for your Pizza'),
+    toppings: yup.array()
+        .max(10,'We can only fit a max of 10 toppings on your Pizza, Sorry.'),
+    quantity: yup.number()
+        .min(1, 'What is the point of ordering 0 Pizzas?'),
+})
+
+export default formSchema;
