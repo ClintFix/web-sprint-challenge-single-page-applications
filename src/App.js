30ae734c4f4a6fc8768d56688c9eb74e9e5a3419
ClintFix <clint.fix@gmail.com>
2021-02-26 08:01:30 -0700

    set up components js files, built out App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
-import React from "react";
+import React, { useState }from "react";
+import { Route, Link, Switch } from 'react-router-dom'
+
+// TO-DO: import components used for the different routes
+// Home
+import Home from './components/Home'
+// Order
+import Order from './components/Order'
+// Confirm
+import Confirmation from './components/Confirmation'
+
+const initialOrder = {
+  Size: '',
+  Sauce: '',
+  Toppings: [],
+  Instructions: '',
+  Quantity: 1,
+}
 
 const App = () => {
+  const [ order, setOrder ] = useState(initialOrder) //setting order as an empty object
+  // const [ price, setPrice ] = useState(0) // setting initial price as 0 (not using until finish the rest)
+
   return (
     <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+      <div className='NavBar'>
+          <div>Lambda Eats</div>
+          <Link to='/'>Home</Link>
+      </div>
+
+      {/* Build a switch with a route to Home, Order, Confirmation */}
+      <Switch>
+        <Route path='/confirmation/:orderID'>
+          <Confirmation order={order} />
+        </Route>
+        <Route path='/order'>
+          <Order order={order} />
+        </Route>
+        <Route path='/'>
+          <Home />
+        </Route>
+      </Switch>
     </>
   );
 };
